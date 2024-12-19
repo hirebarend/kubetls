3dd7b4865557b7721740a569c058f9df48059536
Barend Erasmus <hirebarend@gmail.com>
2024-12-19 14:51:25 +0100

    fix

--- src/server.ts
+++ src/server.ts
@@ -108,18 +108,18 @@ export async function startServer() {
 
   server.route({
     handler: async (request, reply) => {
-      await KUBERNETES_CLIENT_CORE_V1_API.createNamespacedSecret('default', {
-        apiVersion: 'v1',
-        data: {
-          'tls.crt': 'foo',
-          'tls.key': 'bar',
-        },
-        kind: 'Secret',
-        metadata: {
-          name: 'example-com-secret',
-          namespace: 'default',
-        },
-      });
+      // await KUBERNETES_CLIENT_CORE_V1_API.createNamespacedSecret('default', {
+      //   apiVersion: 'v1',
+      //   data: {
+      //     'tls.crt': 'foo',
+      //     'tls.key': 'bar',
+      //   },
+      //   kind: 'Secret',
+      //   metadata: {
+      //     name: 'example-com-secret',
+      //     namespace: 'default',
+      //   },
+      // });
 
       reply.status(200).send();
     },
